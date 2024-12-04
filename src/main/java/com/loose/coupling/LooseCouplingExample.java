0f25bccf6512ccefb39103b7e652914264c51aa7
ashok07-ai <ak850860@gmail.com>
2024-12-04 13:50:01 +0545

    implemented tight coupling and loose coupling example

---
+++ src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -0,0 +1,16 @@
+package com.loose.coupling;
+
+public class LooseCouplingExample {
+    public static void main(String[] args) {
+        // Loosely coupled; We can change the engine type easily
+       UserDataProvider userDataProvider = new UserDatabaseProvider();
+       UserManager userManagerWithDB = new UserManager(userDataProvider);
+       System.out.println(userManagerWithDB.getUserInfo());
+
+       // Loosely coupled
+        UserDataProvider webServiceDataProvider = new WebServiceDataProvider();
+        UserManager userManagerWithWebServiceDB = new UserManager(webServiceDataProvider);
+        System.out.println(userManagerWithWebServiceDB.getUserInfo());
+
+    }
+}
---
+++ src/main/java/com/tight/coupling/UserManager.java
@@ -0,0 +1,11 @@
+package com.tight.coupling;
+
+public class UserManager {
+    // The UserManager class directly creates an instance of UserDatabase class
+    // This creates tight coupling between UserManager and UserDatabase
+    private final UserDatabase userDatabase = new UserDatabase();
+
+    public String getUserInfo(){
+    return userDatabase.getUserDetails();
+    }
+}
